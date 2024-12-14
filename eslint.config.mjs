23954a9906bd190145b4ca9c4dc957f46805032e
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-14 23:42:53 +0800

    Temporarily Disable ESLint Ryles of unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": "off",
+			"react-hooks/exhaustive-deps": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
