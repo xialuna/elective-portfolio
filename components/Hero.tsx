8ca4802a1fe66cf06fa183a2fe5a17fc426e7eb7
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-15 06:01:29 +0800

    Change grid color

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,8 +23,8 @@ export const Hero = () => {
 
 			{/* GRID */}
 			<div
-				className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03
-            ] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0"
+				className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3
+            ] bg-grid-white/[0.02] flex items-center justify-center absolute top-0 left-0"
 			>
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 			</div>
