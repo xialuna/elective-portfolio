b5fa7dcf6352dff501cda55a4c793b750872c211
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-14 22:29:57 +0800

    Create a spotlight and grid component in hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./provider";
 
 const poppins = Poppins({
 	variable: "--font-poppins",
@@ -27,7 +28,14 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${poppins.variable} ${inter.variable} antialiased`}>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark" //Change system to dark
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Hero } from "@/components/Hero";
+
 export default function Home() {
 	return (
 		<main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
 			<div className="max-w-7xl w-full">
-				<h1>Hello World</h1>
+				<Hero />
 			</div>
 		</main>
 	);
