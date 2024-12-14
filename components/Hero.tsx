68d2d15f9a89e03e0931cd46b9b9284c99deccc6
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-14 23:01:24 +0800

    Add text to hero sectiom

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export const Hero = () => {
 	return (
@@ -20,9 +21,30 @@ export const Hero = () => {
 			</div>
 
 			{/* GRID */}
-
-			<div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+			<div
+				className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06
+            ] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0"
+			>
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+			</div>
+
+			{/* HEADER */}
+			<div className="flex justify-center relative my-20 z-10">
+				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+					<h2 className="uppercase tracking-widest text-xs text] flex--center text-blue-100 max-w-80">
+						ELECT IT-E1 Portfolio | PUP-Manila
+					</h2>
+
+					<TextGenerateEffect
+						className="text-center text-[40px] md:text-5xl lg:text-6xl"
+						words="Hi, I'm Xian Hui Cheng"
+					/>
+
+					<p className="text-center md-tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+						I&apos;m a Software Developer focused on building user-focused
+						interfaces
+					</p>
+				</div>
 			</div>
 		</div>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,7 +41,7 @@ const config = {
 				blue: {
 					"100": "#E4ECFF",
 				},
-				purple: "#CBACF9",
+				purple: "#C089EC",
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
 				ring: "hsl(var(--ring))",
