50bb0cbe75494acdd75c0b13f80e8b49a8eb448b
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-16 03:19:11 +0800

    WIP Changed to next.js 14 due to three js not being compatible to next.js 15

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true, // Ensure this is enabled for performance
+};
+
+module.exports = nextConfig;
