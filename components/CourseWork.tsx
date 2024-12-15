3f86996feb572a3f75ba8b3ad6d91faa5817c9c3
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-16 02:13:20 +0800

    WIP

--- components/CourseWork.tsx
+++ components/CourseWork.tsx
@@ -1,4 +1,7 @@
+import { coursework } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/PinContainer";
+import { CardSpotlight } from "./ui/CardSpotlight";
 
 const CourseWork = () => {
 	return (
@@ -6,7 +9,18 @@ const CourseWork = () => {
 			<h1 className="heading">
 				A showcase of my <span className="text-wrappurple">coursework</span>
 			</h1>
-			<div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10"></div>
+			<div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+				{coursework.map(({ id, title, des, img, iconLists, link }) => (
+					<div
+						key={id}
+						className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+					>
+						<PinContainer>
+							<CardSpotlight>{title}</CardSpotlight>
+						</PinContainer>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
---
+++ data/index.ts
@@ -0,0 +1,24 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "CourseWork", link: "#coursework" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const coursework = [
+	{
+		id: 1,
+		title: "Midterms",
+		des: "Lorem Ipsum",
+		img: "/globe.svg",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+		link: "#",
+	},
+	{
+		id: 2,
+		title: "Finals",
+		des: "Lorem Ipsum",
+		img: "/globe.svg",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+		link: "#",
+	},
+];
