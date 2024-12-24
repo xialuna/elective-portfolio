b1f464d4a7665a25e4a62da9cb5b9f88f035d4e4
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-12-24 23:04:15 +0800

    Modify hero section for finals

--- app/finals/page.tsx
+++ app/finals/page.tsx
@@ -1,5 +1,16 @@
+import { Hero } from "@/components/Hero";
 import React from "react";
 
 export default function Finals() {
-	return <div>Finals</div>;
+	return (
+		<main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
+			<div className="max-w-7xl w-full">
+				<Hero
+					heading="Finals Compilation"
+					subHeading="This contains all quizzes, acivities, final exam, and reflection during our finals period."
+					tertiaryHeading="2nd Term"
+				/>
+			</div>
+		</main>
+	);
 }
