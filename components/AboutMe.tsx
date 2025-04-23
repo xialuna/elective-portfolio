ad9ac1069cb793d942237d644c36fa53a1590c73
xialuna <xianhuicheng01@gmail.com>
2025-04-23 14:03:56 +0800

    Add hover state in image with glow

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const AboutMe = () => {
 	return (
-		<div className="py-10 relative container mx-auto">
+		<div className="py-20 relative container mx-auto">
 			<div className="flex justify-center items-center gap-10">
-				<div className="flex-1 space-y-5 ">
+				<div className="flex-1 space-y-3 ">
 					<h2 className="uppercase tracking-widest text-sm text-yellow-300 font-bold">
 						About Me
 					</h2>
@@ -17,7 +17,7 @@ const AboutMe = () => {
 						<span className="text-yellow-200"> Software Developer at CFL </span>
 						and{" "}
 						<span className="text-yellow-200">
-							Freelance Full-stack Developer at Nuworks
+							Freelance Ful-stack Developer at Nuworks
 						</span>
 						, building real-world solutions. <br />
 						<br />
@@ -29,13 +29,15 @@ const AboutMe = () => {
 					</p>
 				</div>
 				<div className="flex-1 flex justify-center">
-					<Image
-						src="/xian_pic.png"
-						alt="Xian's picture"
-						width={400}
-						height={400}
-						className="rounded-md"
-					/>
+					<div className="relative group w-[400px] h-[400px] transition duration-300 ease-in-out">
+						<Image
+							src="/xian_pic.png"
+							alt="Xian's picture"
+							fill
+							className="object-cover rounded-md transition duration-300 group-hover:shadow-[0_0_30px_10px_rgba(255,227,134,0.6)]"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-tr from-yellow-400 via-yellow-300 to-yellow-200 opacity-0 group-hover:opacity-10 transition-opacity duration-300 rounded-md" />
+					</div>
 				</div>
 			</div>
 		</div>
